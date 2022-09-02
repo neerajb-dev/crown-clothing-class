f26dfa6bca8f1a52602b3df1631554b5f230e7ab
Neeraj Bhardwaj <neerajb@yahoo.com>
2022-09-02 23:14:19 +0530

    qwe

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { getRedirectResult } from 'firebase/auth';
+import {
+    auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth
+} from "../../utils/firebase/firebase.utils";
 
 export default class SignIn extends Component {
+
+    componentDidMount() {
+        this.getRedirectResultData();
+    }
+
+    getRedirectResultData = async () => {
+        const response = await getRedirectResult(auth);
+        console.log(response);
+    }
 
     logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -9,13 +24,17 @@ export default class SignIn extends Component {
         const userDocRef = await createUserDocumentFromAuth(user);
     }
 
+    // logGoogleRedirectUser = async () => {
+    //     const { user } = await signInWithGoogleRedirect();
+    //     console.log({ user });
+    // }
+
     render() {
         return (
             <div>
                 <h1>Sign-In Page</h1>
-                <button onClick={this.logGoogleUser}>
-                    Sign-In with Google Pop-Up
-                </button>
+                <button onClick={this.logGoogleUser}>Sign-In with Google Pop-Up</button>
+                <button onClick={signInWithGoogleRedirect}>Sign-In with Google Redirect</button>
             </div>
         )
     }
